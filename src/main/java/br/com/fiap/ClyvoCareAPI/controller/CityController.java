43ca748254e94516ec16beecb0443c87ba125ed3
orlando-IDA <orlandogoncalves8585@gmail.com>
2026-05-18 22:13:33 -0300

    feat: added City Controller.

---
+++ src/main/java/br/com/fiap/ClyvoCareAPI/controller/CityController.java
@@ -0,0 +1,56 @@
+package br.com.fiap.ClyvoCareAPI.controller;
+
+import br.com.fiap.ClyvoCareAPI.dto.CityRequest;
+import br.com.fiap.ClyvoCareAPI.dto.CityResponse;
+import br.com.fiap.ClyvoCareAPI.service.CityService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cidades")
+@RequiredArgsConstructor
+public class CityController {
+    private final CityService cityService;
+
+    @GetMapping
+    public List<CityResponse> findAll() {
+        return cityService.findAll().stream()
+                .map(CityResponse::fromEntity)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public CityResponse findById(@PathVariable Long id) {
+        return CityResponse.fromEntity(cityService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CityResponse> create(@RequestBody @Valid CityRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(CityResponse.fromEntity(cityService.create(request)));
+    }
+
+    @PutMapping("/{id}")
+    public CityResponse update(@PathVariable Long id, @RequestBody @Valid CityRequest request) {
+        return CityResponse.fromEntity(cityService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        cityService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
